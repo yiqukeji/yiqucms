3ca9fbcebcdb64302316341d71c80b825a823b08
jsonxie <1425235569@qq.com>
2018-08-06 23:11:34 +0800

    提交邮箱发送工具类，163邮箱

--- yqyc/src/com/fh/util/mail/SimpleMailSender.java
+++ yqyc/src/com/fh/util/mail/SimpleMailSender.java
@@ -1,59 +1,59 @@
 package com.fh.util.mail;
 
 import java.util.Date;    
-import java.util.Properties;   
 
-import javax.mail.Address;    
-import javax.mail.BodyPart;    
-import javax.mail.Message;    
-import javax.mail.Multipart;    
-import javax.mail.Session;    
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
 import javax.mail.Transport;    
-import javax.mail.internet.InternetAddress;    
-import javax.mail.internet.MimeBodyPart;    
-import javax.mail.internet.MimeMessage;    
-import javax.mail.internet.MimeMultipart;    
+
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.*;
 
 import com.fh.controller.base.BaseController;
 
 /**   
  * 邮件发送器   
- * @author FH QQ 313596790[青苔]
+ * @author 
  */    
-public class SimpleMailSender extends BaseController {    
+public class SimpleMailSender extends BaseController {  
+	private String from = "JFaith2018@163.com"; // 发件人邮箱地址
+	private String user = "JFaith2018@163.com"; // 发件人称号，同邮箱地址
+	private String password = "JFaith2018"; // 发件人邮箱客户端授权码
 /**   
   * 以文本格式发送邮件   
   * @param mailInfo 待发送的邮件的信息   
   */    
-    public boolean sendTextMail(MailSenderInfo mailInfo) throws Exception{    
-      // 判断是否需要身份认证    
-      MyAuthenticator authenticator = null;    
-      Properties pro = mailInfo.getProperties();   
-      if (mailInfo.isValidate()) {    
-      // 如果需要身份认证，则创建一个密码验证器    
-        authenticator = new MyAuthenticator(mailInfo.getUserName(), mailInfo.getPassword());    
-      }   
-      // 根据邮件会话属性和密码验证器构造一个发送邮件的session    
-      Session sendMailSession = Session.getDefaultInstance(pro,authenticator); 
-      logBefore(logger, "构造一个发送邮件的session");
-      // 根据session创建一个邮件消息    
-      Message mailMessage = new MimeMessage(sendMailSession);    
-      // 创建邮件发送者地址    
-      Address from = new InternetAddress(mailInfo.getFromAddress());    
-      // 设置邮件消息的发送者    
-      mailMessage.setFrom(from);    
-      // 创建邮件的接收者地址，并设置到邮件消息中    
-      Address to = new InternetAddress(mailInfo.getToAddress());    
-      mailMessage.setRecipient(Message.RecipientType.TO,to);    
-      // 设置邮件消息的主题    
-      mailMessage.setSubject(mailInfo.getSubject());    
-      // 设置邮件消息发送的时间    
-      mailMessage.setSentDate(new Date());    
-      // 设置邮件消息的主要内容    
-      String mailContent = mailInfo.getContent();    
-      mailMessage.setText(mailContent);    
-      // 发送邮件    
-      Transport.send(mailMessage); 
+    public boolean sendMail(MailSenderInfo mailInfo) throws Exception{    
+    	Properties props = new Properties();
+		props.setProperty("mail.smtp.host", "smtp.163.com"); // 设置发送邮件的邮件服务器的属性（这里使用网易的smtp服务器）
+		props.put("mail.smtp.host", "smtp.163.com"); // 需要经过授权，也就是有户名和密码的校验，这样才能通过验证（一定要有这一条）
+		props.put("mail.smtp.auth", "true"); // 用刚刚设置好的props对象构建一个session
+		Session session = Session.getDefaultInstance(props); // 有了这句便可以在发送邮件的过程中在console处显示过程信息，供调试使
+		session.setDebug(true); // 用session为参数定义消息对象
+		MimeMessage message = new MimeMessage(session); // 加载发件人地址
+		try {
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(mailInfo.getToAddress())); // 加载收件人地址
+			message.setSubject(mailInfo.getSubject()); // 加载标题
+			Multipart multipart = new MimeMultipart(); // 向multipart对象中添加邮件的各个部分内容，包括文本内容和附件
+			BodyPart contentPart = new MimeBodyPart(); // 设置邮件的文本内容
+			contentPart.setContent( mailInfo.getContent(), "text/html;charset=utf-8");
+			multipart.addBodyPart(contentPart);
+			message.setContent(multipart);
+			message.saveChanges(); // 保存变化
+			Transport transport = session.getTransport("smtp"); // 连接服务器的邮箱
+			transport.connect("smtp.163.com", user, password); // 把邮件发送出去
+			transport.sendMessage(message, message.getAllRecipients());
+			transport.close();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			return false;
+		}
       logBefore(logger, "发送成功！");
       return true;    
     }    
@@ -121,12 +121,9 @@ public class SimpleMailSender extends BaseController {
 	     mailInfo.setContent(content);    
 	     //这个类主要来发送邮件   
 	     SimpleMailSender sms = new SimpleMailSender();   
-	     if("1".equals(type)){
-	    	 return sms.sendTextMail(mailInfo);//发送文体格式    
-	     }else if("2".equals(type)){
-	    	 return sms.sendHtmlMail(mailInfo);//发送html格式   
-	     }
-	     return false;
+	     return sms.sendMail(mailInfo);//发送文体格式    
+    	
+
 	   }
     /**
      * @param SMTP  	邮件服务器
@@ -152,11 +149,7 @@ public class SimpleMailSender extends BaseController {
 	     mailInfo.setContent(CONTENT);    
 	     //这个类主要来发送邮件   
 	     SimpleMailSender sms = new SimpleMailSender();
-	    if("1".equals(TYPE)){
-	    	sms.sendTextMail(mailInfo);
-	    }else{
-	    	sms.sendHtmlMail(mailInfo);
-	    }
+	     sms.sendMail(mailInfo);
 	     
 	   }
     
