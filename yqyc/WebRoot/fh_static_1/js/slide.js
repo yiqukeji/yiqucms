c4dc3d410a7e8b27b402daf45ada5e013800d5fe
jsonxie <1425235569@qq.com>
2018-08-07 15:16:08 +0800

    提交新版本

--- yqyc/WebRoot/fh_static_1/js/slide.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$("#slider").responsiveSlides({
-		auto: true,
-		pager: false,
-		nav: true,
-		speed: 1000,
-		namespace: "slide"
-	});
-});
--- yqyc/src/com/fh/util/FileEntity.java
+++
@@ -1,9 +0,0 @@
-//package com.fh.util;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//import org.springframework.web.multipart.MultipartFile;
-//
-//public class FileEntity createFile(MultipartFile multipartFile, HttpServletRequest request) {
-//
-//}
